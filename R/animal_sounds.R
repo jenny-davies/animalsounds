f8033ce5b5435c253d62eeaf0d5a5191c91e4ddb
Davies, Jenny <jenny.davies@mhra.gov.uk>
2025-06-16 10:36:27 +0100

    Added documentation for animal_sounds()

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,13 @@
+#' Print the Sound that an Animal Makes
+#'
+#' @param animal A character string giving the animal
+#' @param sound A character string giving the sound
+#'
+#' @returns A printed string giving the sound the animal makes
+#' @export
+#'
+#' @examples
+#' animal_sounds("cow", "moo")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
